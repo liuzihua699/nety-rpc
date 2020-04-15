bc4064f0e9bcb79c66bc38b15fdc4a1bcb7d0adf
刘子华 <2274298503@qq.com>
2020-04-15 23:07:23 +0800

    使用Jackson实现序列化协议

---
+++ src/main/java/com/zihua/rpc/serializer/JSONSerialization.java
@@ -0,0 +1,39 @@
+package com.zihua.rpc.serializer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+/**
+ * Created by 刘子华.
+ * hs on 2020/04/15.
+ * describe: 序列化协议的默认实现，使用jackson。
+ */
+public class JSONSerialization implements Serialization{
+    
+    private ObjectMapper objectMapper;
+
+    public JSONSerialization() {
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public <T> byte[] serialize(T obj) {
+        try {
+            return objectMapper.writeValueAsBytes(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <T> T deSerialize(Class<T> clazz, byte[] bytes) {
+        try {
+            return objectMapper.readValue(bytes, clazz);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
