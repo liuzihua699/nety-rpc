a94e008090c613271e8bc877c399801f17b84f91
刘子华 <2274298503@qq.com>
2020-04-17 18:13:11 +0800

    Rpc服务器主要逻辑代码

---
+++ src/main/java/com/zihua/rpc/server/RpcServer.java
@@ -0,0 +1,162 @@
+package com.zihua.rpc.server;
+
+import com.zihua.rpc.protocol.*;
+import com.zihua.rpc.registy.ServiceRegistry;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import org.apache.commons.collections4.MapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by 刘子华.
+ * hs on 2020/04/17.
+ * describe:
+ */
+public class RpcServer implements ApplicationContextAware, InitializingBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(RpcServer.class);
+
+    private String serverAddress;
+    private ServiceRegistry serviceRegistry;
+
+    private Map<String, Object> handlerMap = new HashMap<>();
+    private static ThreadPoolExecutor threadPoolExecutor;
+
+    private EventLoopGroup bossGroup = null;
+    private EventLoopGroup workerGroup = null;
+
+    public RpcServer() {
+    }
+
+    public RpcServer(String serverAddress, ServiceRegistry serviceRegistry) {
+        this.serverAddress = serverAddress;
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    /**
+     *  装配所有的 @RpcService 实例
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        Map<String, Object> serviceBeanMap = ctx.getBeansWithAnnotation(RpcService.class);
+        if (MapUtils.isNotEmpty(serviceBeanMap)) {
+            for (Object serviceBean : serviceBeanMap.values()) {
+                String interfaceName = serviceBean.getClass().getAnnotation(RpcService.class).value().getName();
+                logger.info("Loading service: {}", interfaceName);
+                handlerMap.put(interfaceName, serviceBean);
+            }
+        }
+
+        System.out.println("---------------------------------------");
+        for (String key : handlerMap.keySet()) {
+            System.out.println("name:" + key + " " + handlerMap.get(key));
+        }
+        System.out.println("---------------------------------------");
+    }
+    
+    /**
+     *  装配完成后，启动rpc服务
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        start();
+    }
+    
+    public void start() throws Exception {
+        if (bossGroup == null && workerGroup == null) {
+            bossGroup = new NioEventLoopGroup();
+            workerGroup = new NioEventLoopGroup();
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline()
+                                    .addLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 0))
+//                                    .addLast(new RpcDecoder(RpcRequest.class, new JSONSerialization()))
+//                                    .addLast(new RpcEncoder(RpcResponse.class, new JSONSerialization()))
+                                    .addLast(new RpcDecoder(RpcRequest.class))
+                                    .addLast(new RpcEncoder(RpcResponse.class))
+                                    .addLast(new RpcHandler(handlerMap));
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            System.out.println("-------------------------------------------");
+            for (String interfaceName : handlerMap.keySet()) {
+                System.out.println(interfaceName + ":" + handlerMap.get(interfaceName));
+            }
+            System.out.println("-------------------------------------------");
+            
+            String[] array = serverAddress.split(":");
+            String host = array[0];
+            int port = Integer.parseInt(array[1]);
+
+            ChannelFuture future = bootstrap.bind(host, port).sync();
+            logger.info("Server started on port {}", port);
+
+            if (serviceRegistry != null) {
+                serviceRegistry.register(serverAddress);
+            }
+
+            future.channel().closeFuture().sync();
+        }
+    }
+
+
+    public void stop() {
+        if (bossGroup != null) {
+            bossGroup.shutdownGracefully();
+        }
+        if (workerGroup != null) {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    /**
+     *  提交任务
+     */
+    public static void submit(Runnable task) {
+        if (threadPoolExecutor == null) {
+            synchronized (RpcServer.class) {
+                if (threadPoolExecutor == null) {
+                    threadPoolExecutor = new ThreadPoolExecutor(16, 16, 600L,
+                            TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(65536));
+                }
+            }
+        }
+        threadPoolExecutor.submit(task);
+    }
+
+    /**
+     *  添加服务提供者
+     */
+    public RpcServer addService(String interfaceName, Object serviceBean) {
+        if (!handlerMap.containsKey(interfaceName)) {
+            logger.info("Loading service: {}", interfaceName);
+            handlerMap.put(interfaceName, serviceBean);
+        }
+
+        return this;
+    }
+    
+}
