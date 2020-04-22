1521dbdf9b259673fde4a818e7a267a8af76aafc
刘子华 <2274298503@qq.com>
2020-04-22 16:51:26 +0800

    zookeeper常量

---
+++ rpc-client/src/main/java/com/zihua/rpc/registry/Constant.java
@@ -0,0 +1,15 @@
+package com.zihua.rpc.registry;
+
+/**
+ * ZooKeeper constant
+ *
+ * @author huangyong
+ */
+public interface Constant {
+
+    int ZK_SESSION_TIMEOUT = 5000;
+    String ZK_REGISTRY_PATH = "/registry";
+    String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+    int ZK_CONNECTION_TIMEOUT = 5000;
+    String ZK_CHILDREN_PATH = ZK_REGISTRY_PATH + "/data";
+}
