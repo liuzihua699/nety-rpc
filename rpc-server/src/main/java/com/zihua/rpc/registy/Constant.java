8426bc9ab62a032234ccdda13e9c49993d0ebfc2
刘子华 <2274298503@qq.com>
2020-04-22 16:53:15 +0800

    zookeeper连接常量

--- rpc-server/src/main/java/com/zihua/rpc/registy/Constant.java
+++ rpc-server/src/main/java/com/zihua/rpc/registy/Constant.java
@@ -1,14 +1,15 @@
 package com.zihua.rpc.registy;
 
 /**
- * @author by 刘子华.
- * create on 2020/4/17.
- * describe:  注册中心常量表
+ * ZooKeeper constant
+ *
+ * @author huangyong
  */
 public interface Constant {
 
     int ZK_SESSION_TIMEOUT = 5000;
-
     String ZK_REGISTRY_PATH = "/registry";
     String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+    int ZK_CONNECTION_TIMEOUT = 50000;
+    String ZK_CHILDREN_PATH = ZK_REGISTRY_PATH + "/data";
 }
